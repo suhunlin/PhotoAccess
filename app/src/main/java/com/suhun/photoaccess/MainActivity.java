8e4e6c5ed3a0e18a14a000de3babfd80d06fadfa
suhunlin <lin.suhun@msa.hinet.net>
2023-09-13 17:01:58 +0800

    Add layout and initial view

--- app/src/main/java/com/suhun/photoaccess/MainActivity.java
+++ app/src/main/java/com/suhun/photoaccess/MainActivity.java
@@ -3,12 +3,23 @@ package com.suhun.photoaccess;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView img;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void initView(){
+        img = findViewById(R.id.lid_img);
+    }
+
+    public void getImageFromPhotoFun(View view){
+
     }
 }
