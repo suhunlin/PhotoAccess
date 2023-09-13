d48f9d19df5eb19176be9e09b15a632f257a9d86
suhunlin <lin.suhun@msa.hinet.net>
2023-09-13 17:20:30 +0800

    Add logic to check user permission

--- app/src/main/java/com/suhun/photoaccess/MainActivity.java
+++ app/src/main/java/com/suhun/photoaccess/MainActivity.java
@@ -1,25 +1,62 @@
 package com.suhun.photoaccess;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-    ImageView img;
+    private String tag = MainActivity.class.getSimpleName();
+    private ImageView img;
+    private ContentResolver contentResolver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if(checkUserPermissionPhotoAccess()){
+            initContentResolver();
+        }else{
+            requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 123);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == 123){
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                initContentResolver();
+            }else{
+                finish();
+            }
+        }
     }
 
     private void initView(){
         img = findViewById(R.id.lid_img);
     }
 
+    private void initContentResolver(){
+        contentResolver = getContentResolver();
+    }
+
     public void getImageFromPhotoFun(View view){
 
+    }
+
+    private boolean checkUserPermissionPhotoAccess(){
+        boolean result = false;
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)==PackageManager.PERMISSION_GRANTED){
+            result = true;
+        }
+        return result;
     }
 }
